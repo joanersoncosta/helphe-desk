62092f8dc92cf306de05a437da5adeb2447babf7
Joanerson Costa <joanersonc31@gmail.com>
2024-03-19 08:23:02 -0300

    inicia projeto

---
+++ src/main/java/com/wakanda/HelpheDeskApplication.java
@@ -0,0 +1,13 @@
+package com.wakanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpheDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpheDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wakanda/HelpheDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wakanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpheDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
